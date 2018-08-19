e2b58fd39127c409eceb6f11a81730a89252f03f
Shubham Patial <shubh.patial@gmail.com>
2018-08-19 09:10:22 +0530

    second

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,7 +1,14 @@
+// import { Component, OnInit } from '@angular/core';
+// import {FormControl, Validators, FormGroup} from '@angular/forms';
+// import { AuthService } from '../../auth.service';
+// import { Router } from '@angular/router';
+
 import { Component, OnInit } from '@angular/core';
-import {FormControl, Validators, FormGroup} from '@angular/forms';
-import { AuthService } from '../../auth.service';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { first } from 'rxjs/operators';
+
+import { AlertService, UserService } from '../../_services';
 
 // export class MyErrorStateMatcher implements ErrorStateMatcher {
 //   isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
@@ -16,63 +23,118 @@ import { Router } from '@angular/router';
 })
 export class RegisterComponent implements OnInit {
 
+  // isDisabled = true;
+
+  // registerForm: FormGroup;
+  // firstname: string;
+  // lastname: string;
+  // username: string;
+  // password: string;
+  // email: string;
+  // accnumber: number;
+  // dob: Date;
+  // phonenumber: number;
+
+  // response = '';
+
+  // hide = true;
+  // startDate = new Date(1970, 0, 1);
+
+  // emailFormControl = new FormControl('', [
+  //   Validators.required,
+  //   Validators.email,
+  // ]);
+
+  // // matcher = new MyErrorStateMatcher();
+
+  // constructor(private signup: AuthService,
+  //             private router: Router) { }
+
+  // ngOnInit() {
+  //   this.registerForm = new FormGroup({
+  //     'firstname': new FormControl(null, [Validators.required]),
+  //     'lastname': new FormControl(null, [Validators.required]),
+  //     'username': new FormControl(null, [Validators.required]),
+  //     'password': new FormControl(null, [Validators.required]),
+  //     'email': new FormControl(null, [Validators.required, Validators.email]),
+  //     'accnumber': new FormControl(null, [Validators.required]),
+  //     'dob': new FormControl(null, [Validators.required]),
+  //     'phonenumber': new FormControl(null, [Validators.required, Validators.maxLength(10), Validators.minLength(10)])
+  //   });
+  // }
+
+  // onSubmit() {
+  //   const firstname = this.registerForm.value.firstname;
+  //   const lastname = this.registerForm.value.lastname;
+  //   const username = this.registerForm.value.username;
+  //   const password = this.registerForm.value.password;
+  //   const email = this.registerForm.value.email;
+  //   const accnumber = this.registerForm.value.accnumber;
+  //   const dob = this.registerForm.value.dob;
+  //   const phonenumber = this.registerForm.value.phonenumber;
+  //   this.signup.registerUser(firstname, lastname, username, password, email, accnumber, dob, phonenumber)
+  //   .subscribe(
+  //     (data) => {
+  //       // console.log(data);
+  //       if (data) {
+  //         console.log(data);
+  //         this.router.navigate(['/user/1']);
+  //         this.registerForm.reset();
+  //       }
+  //     },
+  //     (error) => {
+  //       console.log(error);
+  //     }
+  //   );
+  // }
+
   registerForm: FormGroup;
-  firstname: string;
-  lastname: string;
-  username: string;
-  password: string;
-  email: string;
-  accnumber: number;
-  dob: Date;
-  phonenumber: number;
+    loading = false;
+    submitted = false;
 
-  hide = true;
-  startDate = new Date(1970, 0, 1);
+    constructor(
+        private formBuilder: FormBuilder,
+        private router: Router,
+        private userService: UserService,
+        private alertService: AlertService) { }
 
-  emailFormControl = new FormControl('', [
-    Validators.required,
-    Validators.email,
-  ]);
+    ngOnInit() {
+        this.registerForm = this.formBuilder.group({
+            'firstName': ['', Validators.required],
+            'lastName': ['', Validators.required],
+            'username': ['', Validators.required],
+            'password': ['', [Validators.required, Validators.minLength(6)]],
+            'email': ['', Validators.required, Validators.email],
+            'accnumber': ['', Validators.required],
+            'dob': ['', Validators.required],
+            'phonenumber': ['', Validators.required, Validators.maxLength(10), Validators.minLength(10)]
+        });
+    }
 
-  // matcher = new MyErrorStateMatcher();
+    // convenience getter for easy access to form fields
+    get f() { return this.registerForm.controls; }
 
-  constructor(private signup: AuthService,
-              private router: Router) { }
+    onSubmit() {
+        this.submitted = true;
 
-  ngOnInit() {
-    this.registerForm = new FormGroup({
-      'firstname': new FormControl(null, [Validators.required]),
-      'lastname': new FormControl(null, [Validators.required]),
-      'username': new FormControl(null, [Validators.required]),
-      'password': new FormControl(null, [Validators.required]),
-      'email': new FormControl(null, [Validators.required, Validators.email]),
-      'accnumber': new FormControl(null, [Validators.required]),
-      'dob': new FormControl(null, [Validators.required]),
-      'phonenumber': new FormControl(null, [Validators.required, Validators.maxLength(10), Validators.minLength(10)])
-    });
-  }
+        // stop here if form is invalid
+        // if (this.registerForm.invalid) {
+        //     return;
+        // }
 
-  onSubmit() {
-    const firstname = this.registerForm.value.firstname;
-    const lastname = this.registerForm.value.lastname;
-    const username = this.registerForm.value.username;
-    const password = this.registerForm.value.password;
-    const email = this.registerForm.value.email;
-    const accnumber = this.registerForm.value.accnumber;
-    const dob = this.registerForm.value.dob;
-    const phonenumber = this.registerForm.value.phonenumber;
-    this.signup.registerUser(firstname, lastname, username, password, email, accnumber, dob, phonenumber)
-    .subscribe(
-      (data) => {
-        console.log(data);
-        if (data.success) {
-          this.router.navigate(['/user/:id']);
-        }
-      },
-      (error) => {
-        console.log(error);
-      }
-    );
-  }
+        this.loading = true;
+        console.log(this.registerForm.value);
+        this.userService.register(this.registerForm.value)
+            .pipe(first())
+            .subscribe(
+                data => {
+                    this.alertService.success('Registration successful', true);
+                    this.router.navigate(['/login']);
+                },
+                error => {
+                    this.alertService.error(error);
+                    this.loading = false;
+                });
+    }
 
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../_services';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authenticationService: AuthenticationService ) {}
 
   ngOnInit() {
+  }
+
+  onLogOut() {
+    this.authenticationService.logout();
   }
 
 }
